d400b8a0b8abcc523b3a78131211d79584e3fe1c
chunsam <chunsam@blocko.io>
2023-07-27 23:51:35 +0900

    변수와 타입 출력

--- kotlin-playground/src/main/kotlin/Main.kt
+++ kotlin-playground/src/main/kotlin/Main.kt
@@ -1,6 +1,14 @@
+
 fun main(args: Array<String>) {
-    val prac: String =
-        """반가워요
-            |어그래반갑다.""".trimMargin()
-    print(prac)
+    println("""안녕하세요, Shoppi 에 오신 것을 환영합니다!
+        |쇼핑을 계속 하시려면 이름을 입력햇주세요 :)
+    """.trimMargin())
+    val inputName = readlnOrNull()
+    println("""
+        감사합니다. 반갑습니다, $inputName 님
+        원하시는 카테고리를 입력해주세요.
+        ***===============================***
+        """.trimIndent())
+
+
 }
