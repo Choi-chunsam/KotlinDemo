d1df88b95489f7faf1f8048bb787924d0bd613d1
chunsam <chunsam@blocko.io>
2023-08-19 00:03:33 +0900

    몽고 삭제

--- kotlinMongoDemo/src/main/kotlin/my/mongo/kotlinmongodemo/config/MongoConfig.kt
+++
@@ -1,4 +0,0 @@
-package my.mongo.kotlinmongodemo.config
-
-class MongoConfig {
-}
--- kotlinMongoDemo/src/test/kotlin/my/mongo/kotlinmongodemo/domain/company/service/CompanyServiceTest.kt
+++
@@ -1,5 +0,0 @@
-package my.mongo.kotlinmongodemo.domain.company.service
-
-import org.junit.jupiter.api.Assertions.*
-
-class CompanyServiceTest
